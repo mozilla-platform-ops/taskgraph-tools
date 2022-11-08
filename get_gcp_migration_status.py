0a192b22609f5496d1fb6314b600c699a5c621ed
Andrew Erickson <aerickson@mozilla.com>
2022-11-08 15:00:26 -0800

    rework

--- get_gcp_migration_status.py
+++ get_gcp_migration_status.py
@@ -2,25 +2,36 @@
 
 from subprocess import run
 
-gcp_command = "./worker_report.py | grep gcp | wc -l" #.split(' ')
-gcp_p = run(gcp_command, capture_output=True, check=True, shell=True)
-
-total_command = "./worker_report.py | \
-        grep -v releng-hardware | \
-        grep -v autophone | \
-        grep -v win | \
-        grep -v scriptworker | \
-        grep -v macosx | wc -l" #.split(' ')
-total_p = run(total_command, capture_output=True, check=True, shell=True)
+# not that useful (background) vaules
 
 total_all_command = "./worker_report.py | wc -l"
 total_all_p = run(total_all_command, capture_output=True, check=True, shell=True)
 
-g_val = int(gcp_p.stdout.decode().strip())
+gcp_command = "./worker_report.py | grep gcp | wc -l" #.split(' ')
+gcp_p = run(gcp_command, capture_output=True, check=True, shell=True)
+
+# useful values
+
+# gcp-migrate-able tasks
+cmd_migratable = "./worker_report.py | \
+        grep -v releng-hardware | \
+        grep -v autophone | \
+        grep -v win | \
+        grep -v 'built-in/succeed' | \
+        grep -v scriptworker | \
+        grep -v macosx"
+# gcp-migrate-able tasks on gcp
+cmd_migratable_not_on_gcp = f"{cmd_migratable} | grep gcp" 
+
+total_p = run(f"{cmd_migratable} | wc -l", capture_output=True, check=True, shell=True)
+migrated_p = run(f"{cmd_migratable_not_on_gcp} | wc -l", capture_output=True, check=True, shell=True)
+
+g_val = int(migrated_p.stdout.decode().strip())
 t_val = int(total_p.stdout.decode().strip())
 t_all_val = int(total_all_p.stdout.decode().strip())
 
-print(f"gcp jobs: {g_val}")
-print(f"gcp migrate-able jobs (non-win, non-macosx, non-bitbar, non-releng-hardware, non-scriptworker jobs): {t_val}")
-print(f"all jobs: {t_all_val}")
-print(f"percentage of migrateable jobs migrated to gcp: {g_val}/{t_val} = {g_val / t_val}")
+print(f"( all tasks: {t_all_val} )")
+print(f"gcp-migratable tasks: {t_val}")
+print("  (non-win, non-macosx, non-bitbar, non-releng-hardware, non-scriptworker, non-built-in tasks)")
+print(f"gcp-migrateable tasks on gcp: {g_val}" )
+print(f"percentage of migratable tasks migrated to gcp: {g_val}/{t_val} = {g_val / t_val}")
